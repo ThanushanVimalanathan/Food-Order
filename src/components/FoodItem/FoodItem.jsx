6f6bcf9bdc9861db02b9e9b403f22b609b93afea
VimalanathanThanushan <thanushaan69@gmail.com>
2025-09-26 11:22:17 +0530

    Feat:cart functionality

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './FoodItem.css'
+import { assets } from '../../assets/frontend_assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
-const FoodItem = () => {
+const FoodItem = ({id,name,price,description,image}) => {
+
+  
+  const {cartItems,addToCard,removeFromCart} = useContext(StoreContext);
+
   return (
-    <div>
-      
+    <div className='food-item'>
+      <div className="food-item-img-container">
+        <img className='food-item-image' src={image} alt="" />
+        {
+          !cartItems[id]?
+          <img className='add' onClick={()=>addToCard(id)} src={assets.add_icon_white} alt='' />
+          :<div className='food-item-counter'>
+            <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+            <p>{cartItems[id]}</p>
+            <img onClick={()=>addToCard(id)} src={assets.add_icon_green} alt="" />
+          </div>
+        }
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
+        </div>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
     </div>
   )
 }
