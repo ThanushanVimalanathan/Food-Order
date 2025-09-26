622d9e743b62928e66e85284884792f19e857006
VimalanathanThanushan <thanushaan69@gmail.com>
2025-09-27 00:16:21 +0530

    Feat:fulli completed UI

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/frontend_assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className='footer-content-left'>
+                <img src={assets.logo} alt="" />
+                <p className=''>Tomato is your ultimate food delivery partner. We've partnered with the best local restaurants to bring your favorite cuisines to your doorstep. Enjoy a seamless ordering experience, real-time tracking, and quick delivery. Your satisfaction is our main ingredient.</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className='footer-content-center'>
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className='footer-content-right'>
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+94-077-547-4489</li>
+                    <li>contact@tomato.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">Coppyright 2025 © Tomato.com - All Right Reserved.</p>
+      
+    </div>
+  )
+}
+
+export default Footer
